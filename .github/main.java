fdc6bbdc978b0da14d885633d1854f5bccdfdba7
Andrew Niebla <113561001+AndrewNiebla@users.noreply.github.com>
2022-09-21 23:02:59 +0000

    moved around the code lines

--- .github/main.java
+++ .github/main.java
@@ -1,6 +1,6 @@
 // make this print HW
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
+public static void main(String[] args) {
+System.out.println("Hello World");
 }
 }
